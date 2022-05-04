099e7cca595dabbfea10db284aa105ea34e0ad76
takuma_inoue <inoue.takuma@pro.headwaters.co.jp>
2022-05-04 23:01:31 +0900

    add input form

--- index.php
+++ index.php
@@ -1,32 +1,33 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>ToDo App</h1>
     <h2>現在の日付</h2>
-    <p>履歴</p>
-    <h2>ToDoリスト</h2>
-
     <?php
-        // DBに接続
-        $pdo = new PDO("mysql:host=localhost;dbname=lesson;charset-utf8", "root", "root");
-
-        // データの取得
-        $sql = "SELECT * FROM todolist ORDER BY update_at;";
-        $stmt = $pdo->prepare($sql);
-        $stmt -> execute();
-
-        // データの表示
-        $row = $stmt -> fetch(PDO::FETCH_ASSOC);
-        print_r($row);
-        echo ("<br/>");
-        
+        echo date('Y/m/d')."<br/>\n";
     ?>
 
+    <p><a href="#">履歴</a></p>
+    <h2>投稿フォーム</h2>
+    <form action="index.php" method="post">
+        <label>投稿内容</label>
+        <input type="text" name="content">
+        <button type="submit">投稿</button>
+    </form>
+
+    <?php
+        $content = $_POST["content"];
+        echo "投稿内容：". $content;
+    ?>
+
+    <h2>ToDoリスト</h2>
     <table>
         <tr>
             <th>ID</th>
@@ -43,5 +44,21 @@
             <td><a href="#">詳細</a></td>
         </tr>
     </table>
+
+    <?php
+    // DBに接続
+        $pdo = new PDO("mysql:host=localhost;dbname=lesson;charset-utf8", "root", "root");
+    // データの取得
+        $sql = "SELECT * FROM todolist ORDER BY update_at;";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+    // データの表示
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            print_r($row);
+            echo ("<br/>");
+        }
+    ?>
+
 </body>
+
 </html>
