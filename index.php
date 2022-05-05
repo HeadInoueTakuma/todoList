6db059d1389302a011648771b77bd1b910258faf
takuma_inoue <inoue.takuma@pro.headwaters.co.jp>
2022-05-05 13:20:56 +0900

    add exception

--- index.php
+++ index.php
@@ -11,21 +11,51 @@
     <h1>ToDo App</h1>
     <h2>現在の日付</h2>
     <?php
-        echo date('Y/m/d')."<br/>\n";
+    echo date('Y/m/d') . "<br/>\n";
     ?>
 
     <p><a href="#">履歴</a></p>
     <h2>投稿フォーム</h2>
+
+
+    <?php
+
+    //例外処理
+    try {
+         // DBに接続するためのPDOオブジェクトを作成
+        $pdo = new PDO("mysql:host=localhost;dbname=lesson;charset-utf8", "root", "root");
+        
+            $content = $_POST["content"];
+    
+            // DBに書き込みたい形にsqlを指定
+            $sql  = "INSERT INTO todolist (content, updated_at) VALUES (:content, NOW());";
+    
+            // $stmt変数にprepareメソッドを実行
+            $stmt = $pdo->prepare($sql);
+    
+            // bindValueでsqlに渡す値を指定
+            // bindValueはプリペアドステートメントで使用するSQL文の中で変数の値を紐づける為の関数
+            // プリペアドステートメントはSQL文に対して変更が効くようにする為の機能
+            // $stmtはphpでの慣習でstatementの略
+            $stmt->bindValue(":content", $content, PDO::PARAM_STR);
+    
+            // executeメソッドでプリペアドステートメントを実行する
+            $stmt->execute();
+    } catch (PDOException $e) {
+        //エラー出力
+        echo "データベースエラー（PDOエラー）";
+        //エラーの詳細を調べたい時はコメントアウトを外す
+        var_dump($e->getMessage());
+    }
+    // print_r($_POST);
+    ?>
+
+
     <form action="index.php" method="post">
         <label>投稿内容</label>
         <input type="text" name="content">
         <button type="submit">投稿</button>
     </form>
-
-    <?php
-        $content = $_POST["content"];
-        echo "投稿内容：". $content;
-    ?>
 
     <h2>ToDoリスト</h2>
     <table>
@@ -44,21 +74,6 @@
             <td><a href="#">詳細</a></td>
         </tr>
     </table>
-
-    <?php
-    // DBに接続
-        $pdo = new PDO("mysql:host=localhost;dbname=lesson;charset-utf8", "root", "root");
-    // データの取得
-        $sql = "SELECT * FROM todolist ORDER BY update_at;";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute();
-    // データの表示
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            print_r($row);
-            echo ("<br/>");
-        }
-    ?>
-
 </body>
 
 </html>
